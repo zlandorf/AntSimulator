68d606d1f40359f083d2b69a1927de8f1fb0267d
zlandorf <paul.milian@hotmail.fr>
2013-12-20 23:20:41 +0100

    cancelling changes

--- antSimulator/src/fr/zlandorf/antSimulator/art/AntSimulatorArt.java
+++ antSimulator/src/fr/zlandorf/antSimulator/art/AntSimulatorArt.java
@@ -29,13 +29,9 @@ public class AntSimulatorArt {
 		int width = texture.getWidth() / nbSlices;
 		int height = texture.getHeight();
 		
-		
-		
 		for (int i = 0; i < nbSlices; i++) {
 			foodSourceTexture[i] = new TextureRegion(texture, i * width, 0, width, height);
 		}
-		
-		//test merge gitgui/gitk
 	}
 
 	private static void loadNestTexture() {
